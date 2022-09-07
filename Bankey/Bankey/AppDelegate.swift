df0c14e17593d7237681f021138d57bf12f03b82
Eugenio Leal <eugenioleal333@gmail.com>
2022-09-07 08:22:30 -0500

    feat: Added Tab Bar

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   let loginViewController = LoginViewController()
   let onboardingContainerViewController = OnboardingContainerViewController()
   let dummyViewController = DummyViewController()
+  let mainViewController = MainViewController()
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     window = UIWindow(frame: UIScreen.main.bounds)
@@ -21,7 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     loginViewController.delegate = self
     onboardingContainerViewController.delegate = self
     dummyViewController.logoutDelegate = self
-    window?.rootViewController = loginViewController
+    window?.rootViewController = mainViewController
+    mainViewController.selectedIndex = 1
     return true
   }
 }
---
+++ Bankey/Bankey/MainViewController.swift
@@ -0,0 +1,101 @@
+//
+//  MainViewController.swift
+//  Bankey
+//
+//  Created by Eugenio Leal on 07/09/22.
+//
+
+import UIKit
+
+let appColor: UIColor = .systemTeal
+
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupTabBar()
+    }
+
+    private func setupViews() {
+        let summaryVC = AccountSummaryViewController()
+        let moneyVC = MoveMoneyViewController()
+        let moreVC = MoreViewController()
+
+        summaryVC.setTabBarImage(imageName: "list.dash.header.rectangle", title: "Summary")
+        moneyVC.setTabBarImage(imageName: "arrow.left.arrow.right", title: "Move Money")
+        moreVC.setTabBarImage(imageName: "ellipsis.circle", title: "More")
+
+        let summaryNC = UINavigationController(rootViewController: summaryVC)
+        let moneyNC = UINavigationController(rootViewController: moneyVC)
+        let moreNC = UINavigationController(rootViewController: moreVC)
+
+        summaryNC.navigationBar.barTintColor = appColor
+        hideNavigationBarLine(summaryNC.navigationBar)
+        
+        let tabBarList = [summaryNC, moneyNC, moreNC]
+
+        viewControllers = tabBarList
+    }
+    
+    private func hideNavigationBarLine(_ navigationBar: UINavigationBar) {
+        let img = UIImage()
+        navigationBar.shadowImage = img
+        navigationBar.setBackgroundImage(img, for: .default)
+        navigationBar.isTranslucent = false
+    }
+    
+    private func setupTabBar() {
+        tabBar.tintColor = appColor
+        tabBar.isTranslucent = false
+    }
+}
+
+class AccountSummaryViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemGreen
+    }
+}
+
+class MoveMoneyViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemOrange
+    }
+}
+
+class MoreViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemPurple
+    }
+}
+
+import UIKit
+
+extension UIViewController {
+    // Old - deprecated
+//    func setStatusBar() {
+//        let statusBarSize = UIApplication.shared.statusBarFrame.size // deprecated but OK
+//        let frame = CGRect(origin: .zero, size: statusBarSize)
+//        let statusbarView = UIView(frame: frame)
+
+//        statusbarView.backgroundColor = appColor
+//        view.addSubview(statusbarView)
+//    }
+    
+    // New
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
