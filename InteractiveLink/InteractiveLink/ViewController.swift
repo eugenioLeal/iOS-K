63d1ade16ae7017abf18f1b9afb518b31b182248
Eugenio Leal <eugenioleal333@gmail.com>
2022-09-22 19:41:05 -0500

    general: made a checklist to create a xcode project and remove storyboards. also created a project that handles links and urls from UILabel (empty project)

--- InteractiveLink/InteractiveLink/ViewController.swift
+++ InteractiveLink/InteractiveLink/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController, UITextViewDelegate {
     let stackView = UIStackView()
     let textView = UITextView()
     
-    let str: NSString = "He leído, entiendo y acepto estar sujeto a los Términos y Condiciones de Alpaca Securities LLC y Sherpa Brokers LLC y a todos los otros términos, divulgaciones y afirmaciones aplicadas a mi en relación al Acuerdo de Cliente de Alpaca. También reconozco que el Acuerdo de Cliente de Alpaca. También reconozco que el acuerdo de Cliente de Alpaca contiene una clausula de  arbitraje previo a disputa en la Sección 43. Así como el Aviso de Privacidad de Rapyd.";
+    let str: String = "He leído, entiendo y acepto estar sujeto a los Términos y Condiciones de Alpaca Securities LLC y Sherpa Brokers LLC y a todos los otros términos, divulgaciones y afirmaciones aplicadas a mi en relación al Acuerdo de Cliente de Alpaca. También reconozco que el Acuerdo de Cliente de Alpaca. También reconozco que el acuerdo de Cliente de Alpaca contiene una clausula de  arbitraje previo a disputa en la Sección 43. Así como el Aviso de Privacidad de Rapyd.";
     
     
     override func viewDidLoad() {
@@ -29,16 +29,23 @@ extension ViewController {
         stackView.axis = .vertical
         stackView.spacing = 20
         
-        let tosRange: NSRange = str.range(of: "Términos y Condiciones")
-        let privRange: NSRange = str.range(of: "Aviso de Privacidad")
+        let tosRange: NSRange
         let rootAttStr = NSMutableAttributedString(string: String(str))
-        rootAttStr.addAttribute(.link, value: "https://tos.com", range: tosRange)
-        rootAttStr.addAttribute(.link, value: "https://google.com", range: privRange)
+        if let tosR: Range = str.range(of: "Términos y Condiciones") {
+            tosRange = NSRange(tosR, in: str)
+            rootAttStr.addAttribute(.link, value: "https://tos.com", range: tosRange)
+        }
+        let privRange: NSRange
+        if let privR: Range = str.range(of: "Aviso de Privacidad") {
+            privRange = NSRange(privR, in: str)
+            rootAttStr.addAttribute(.link, value: "https://google.com", range: privRange)
+        }
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.delegate = self
         textView.attributedText = rootAttStr
         textView.font = UIFont.preferredFont(forTextStyle: .body)
         textView.textAlignment = .center
+        textView.isSelectable = true
         textView.isEditable = false
         textView.isScrollEnabled = false
         textView.backgroundColor = .clear
